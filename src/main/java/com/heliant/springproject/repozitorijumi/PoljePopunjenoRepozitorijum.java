2e3c5e96b5876315f9f24940f60585ee54fdd943
Dusan <batinicadusan819@gmail.com>
2024-02-25 22:41:17 +0100

    Added missing @Repository annotations

--- src/main/java/com/heliant/springproject/repozitorijumi/PoljePopunjenoRepozitorijum.java
+++ src/main/java/com/heliant/springproject/repozitorijumi/PoljePopunjenoRepozitorijum.java
@@ -2,7 +2,9 @@ package com.heliant.springproject.repozitorijumi;
 
 import com.heliant.springproject.entiteti.PoljePopunjeno;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PoljePopunjenoRepozitorijum extends JpaRepository<PoljePopunjeno, Long> {
 
 }
--- src/main/java/com/heliant/springproject/repozitorijumi/StatistikaRepozitorijum.java
+++ src/main/java/com/heliant/springproject/repozitorijumi/StatistikaRepozitorijum.java
@@ -2,7 +2,9 @@ package com.heliant.springproject.repozitorijumi;
 
 import com.heliant.springproject.entiteti.Statistika;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StatistikaRepozitorijum extends JpaRepository<Statistika, Long> {
 
 }
