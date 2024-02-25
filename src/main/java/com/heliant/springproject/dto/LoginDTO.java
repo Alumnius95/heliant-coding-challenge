66c5b652cc5caf77f625833ede64abedb7b7ec7d
Dusan <batinicadusan819@gmail.com>
2024-02-25 01:21:45 +0100

    Added getters and setters for LoginDTO

--- src/main/java/com/heliant/springproject/dto/LoginDTO.java
+++ src/main/java/com/heliant/springproject/dto/LoginDTO.java
@@ -1,10 +1,14 @@
 package com.heliant.springproject.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginDTO {
     private String korisnickoIme;
     private String lozinka;
